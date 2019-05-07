f3b539f4edfc1b29a5a8e2cb1bbd58cd9d54fb12
John Bero <john.bero@relativity.com>
2019-05-07 17:24:16 -0500

    Uploading old completed projects

---
+++ 30DaysOfCoding/Day 14/Hackerrank_30DaysOfCoding_Day14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] a)
+    {
+        elements = a;
+        maximumDifference = 0;
+    }
+
+    public int computeDifference()
+    {
+        for (int i = 0; i < elements.Length; i++)
+        {
+            int dif = 0;
+            for (int k = 0; k < elements.Length; k++)
+            {
+                if (i != k)
+                {
+                    dif = Math.Abs(elements[i] - elements[k]);
+                }
+                if (maximumDifference < dif)
+                {
+                    maximumDifference = dif;
+                }
+            }
+        }
+
+        return maximumDifference;
+    }
+
+} // End of Difference Class
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+        Console.ReadLine();
+    }
+}
