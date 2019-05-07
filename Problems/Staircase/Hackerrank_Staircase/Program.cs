75240b9993bf59415bf14ceca316f54fe82590fb
John Bero <john.bero@relativity.com>
2019-05-07 17:40:39 -0500

    Still more old projects.

---
+++ Problems/Staircase/Hackerrank_Staircase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the staircase function below.
+    static void staircase(int n)
+    {
+        for(int i = n-1; i >= 0; i--)
+        {
+            string output = "";
+            for (int k = 0; k <= n-1; k++)
+            {
+                if (k < i)
+                {
+                    output += " ";
+                }
+                else
+                {
+                    output += "#";
+                }
+            }
+            Console.WriteLine(output);
+        }
+        Console.ReadLine();
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
