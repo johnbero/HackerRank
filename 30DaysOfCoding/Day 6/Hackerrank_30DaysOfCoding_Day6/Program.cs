a893b0385ed79333a10868fda78ca1f4264e9e5e
John Bero <john.bero@relativity.com>
2019-05-07 17:32:11 -0500

    Uploading more old projects.

---
+++ 30DaysOfCoding/Day 6/Hackerrank_30DaysOfCoding_Day6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    public string GetEvensAndOdds(string s)
+    {
+        string temp0 = string.Empty;
+        string temp1 = string.Empty;
+        string output = string.Empty;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                temp0 += s[i];
+            }
+            else
+            {
+                temp1 += s[i];
+            }
+        }
+
+        output = temp0 + " " + temp1;
+
+        return output;
+    }
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int t = Convert.ToInt32(Console.ReadLine());
+        string s;
+        Solution output = new Solution();
+
+        for (int i = 0; i < t; i++)
+        {
+            s = Console.ReadLine();
+            Console.WriteLine(output.GetEvensAndOdds(s));
+        }
+
+        Console.ReadLine();
+    }
+}
+
---
+++ 30DaysOfCoding/Day 7/Hackerrank_30DaysOfCoding_Day7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        string output = string.Empty;
+
+        for(int i = n-1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                output += arr[i];
+            }
+            else
+            {
+                output += arr[i] + " ";
+            }
+        }
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+}
